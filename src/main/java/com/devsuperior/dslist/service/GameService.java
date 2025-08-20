824e319d57496233847af0406237a3e014cb061f
Camila <alvescamila87@gmail.com>
2025-08-20 19:49:19 -0300

    feat: get by id

--- src/main/java/com/devsuperior/dslist/service/GameService.java
+++ src/main/java/com/devsuperior/dslist/service/GameService.java
@@ -1,10 +1,12 @@
 package com.devsuperior.dslist.service;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -17,6 +19,14 @@ public class GameService {
     public List<GameMinDTO> findAll() {
         List<Game> result = repository.findAll();
         List<GameMinDTO> dto = result.stream().map(GameMinDTO::new).toList();
+        return dto;
+    }
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Game result = repository.findById(id).orElseThrow(() -> new IllegalArgumentException("ID não encontrado."));
+        GameDTO dto = new GameDTO(result);
+
         return dto;
     }
 }
