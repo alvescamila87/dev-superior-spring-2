6f8e423fdf55b7c06c010cc0076805fad819155a
Camila <alvescamila87@gmail.com>
2025-08-20 19:58:49 -0300

    feat: get list

---
+++ src/main/java/com/devsuperior/dslist/controllers/GameListController.java
@@ -0,0 +1,23 @@
+package com.devsuperior.dslist.controllers;
+
+import com.devsuperior.dslist.dto.GameListDTO;
+import com.devsuperior.dslist.service.GameListService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/list")
+public class GameListController {
+
+    @Autowired
+    private GameListService service;
+
+    @GetMapping
+    public List<GameListDTO> findAll() {
+        return service.findAll();
+    }
+}
